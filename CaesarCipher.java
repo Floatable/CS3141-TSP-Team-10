9a9599be3cbbc16885a048b912d45d1d6d9f0d3c
joshu <joshua.dvd.carson@gmail.com>
2025-03-24 17:17:36 -0400

    modified:   CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -18,7 +18,7 @@ public class CaesarCipher {
                 updatedChar -= 26;
             }
             //add a space when necessary
-            if (updatedChar - key == 32) {
+            if (inputList[i] == 32) {
                 output += " ";
             }
             //otherwise, update the output
@@ -32,9 +32,45 @@ public class CaesarCipher {
     
     }
 
+    static String decode (String input, int key) {
+
+        //initialize the output
+        String output = "";
+        input = input.toLowerCase();
+        //convert the input string to an array of chars
+        char[] inputList = input.toCharArray();
+
+        //iterate through the array of chars
+        for (int i = 0; i < inputList.length; i++) {
+
+            //find the current character as an int
+            int updatedChar = (int)inputList[i] - key;
+            //loop back around to "a" if the loop surpasses the end of the alphabet
+            if (updatedChar <= 96) {
+                updatedChar += 26;
+            }
+
+            //add a space when necessary
+            if (inputList[i] == 32) {
+                output += " ";
+            }
+            //otherwise, update the output
+            else {
+                output += (char)updatedChar;
+            }
+
+        }
+
+        return output;
+
+    }
+
     public static void main(String[] args) {
         
         String result = encode("the worst cipher", 7);
+        System.out.println(result);
+
+        result = decode("aol dvyza jpwoly", 7);
         System.out.println(result);
 
     }
